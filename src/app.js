9b2bfb11eeb6184c06c891ef3d54eadf8fa655a7
graceyiyo-cloud <graceyiyo@gmail.com>
2026-09-15 21:16:13 +0800

    Align book actions with synopsis control

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import { newId, copyText } from "./browser-utils.js";
-import { renderPage, renderLayer } from "./views.js?v=20260915-tags";
+import { renderPage, renderLayer } from "./views.js?v=20260915-actions";
 import { createDemoRepository, loadCloud, lookupBook } from "./repository.js";
 import {
   DEFAULT_CATEGORIES,
