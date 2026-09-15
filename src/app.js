4eec9644cc7803b5334d44debe6eabb93016c8d2
graceyiyo-cloud <graceyiyo@gmail.com>
2026-09-15 20:48:00 +0800

    Highlight pinned and currently reading books

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import { newId, copyText } from "./browser-utils.js";
-import { renderPage, renderLayer } from "./views.js?v=20260915-launch";
+import { renderPage, renderLayer } from "./views.js?v=20260915-status";
 import { createDemoRepository, loadCloud, lookupBook } from "./repository.js";
 import {
   DEFAULT_CATEGORIES,
